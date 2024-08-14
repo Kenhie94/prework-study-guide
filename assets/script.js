3d39f9bc2a1a14973a9323e059255cfd6cb4b26e
Kenjy Jap <kenhiebusiness@gmail.com>
2024-08-14 13:49:16 -0700

    Implemented JavaScript onto the App

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+var shape = "circle";
+
+var topic = ["HTML", "CSS", "Git", "JavaScript"];
+
+for (let i = 0; i < topic.length; i++) {
+  if (topic[i] === "HTML") {
+    console.log("Let's study HTML!");
+  } else if (topic[i] === "CSS") {
+    console.log("Let's study CSS!");
+  } else if (topic[i] === "Git") {
+    console.log("Let's study Git!");
+  } else if (topic[i] === "JavaScript") {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log("Please try again!");
+  }
+}
